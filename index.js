a5bd2378728fa62ee68d2022fd28a68865f9ad5c
Santeri Liukkonen <sliukkonen@Santeris-MacBook-Pro.local>
2018-10-01 21:42:46 +0200

    Added workers implementation

--- index.js
+++ index.js
@@ -2,11 +2,27 @@
     const express = require("express");
     const crypto = require("crypto");
     const app = express(); 
+    const Worker = require("webworker-threads").Worker;
 
     app.get("/", (req, res) => {
-        crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
-            res.send("Hi there");
+
+        const worker = new Worker(function() {
+            this.onmessage = function() {
+                let counter = 0;
+                while (counter < 1e9) {
+                    counter++;
+                }
+
+                this.postMessage(counter);
+            }
         });
+
+        worker.onmessage = function(message) {
+            console.log(message.data);
+            res.send("" + message.data);
+        };
+
+        worker.postMessage(); 
     });
 
     app.get("/fast", (req, res) => {
